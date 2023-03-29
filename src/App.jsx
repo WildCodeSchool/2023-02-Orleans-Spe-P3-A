bd41f73cc8c990e22cb25521cc7ec6aa12693a2e
SILVESTRI_GREGORY <gregory.silvestri45@gmail.com>
2023-03-29 11:58:58 +0200

    list Filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
-import { useState } from 'react';
-import './App.css';
+import List from './components/ListChampions';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <main>
-      <h1>{'Hey les spé React !'}</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-    </main>
-  );
+  return <List />;
 }
 
 export default App;
