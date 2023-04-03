060921eb35693eb9c41f51b20967498c802aed7f
SILVESTRI_GREGORY <gregory.silvestri45@gmail.com>
2023-04-03 10:45:13 +0200

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
 import List from './components/ListChampions';
+import Navbar from './components/Navbar';
 
 function App() {
-  return <List />;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<div>Hello World!</div>} />
+        <Route path='/champions' element={<List />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import logo from '../assets/images/blizzard.png';
+import {
+  Box,
+  Flex,
+  Link as ChakraLink,
+  IconButton,
+  useDisclosure,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerBody,
+} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { HamburgerIcon } from '@chakra-ui/icons';
+
+const Navbar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <Box bg='gray.800'>
+      <Flex alignItems='center' justifyContent='space-between' py='3'>
+        <Box fontWeight='bold' color='white' ml='4'>
+          <img src={logo} alt='Logo' width='150px' />
+        </Box>
+        <Box display={['none', 'block']} mr='4'>
+          <ChakraLink as={Link} to='/' color='white' fontSize='20px' mr='4'>
+            Accueil
+          </ChakraLink>
+          <ChakraLink as={Link} to='/champions' color='white' fontSize='20px' mr='4'>
+            Champions
+          </ChakraLink>
+          <ChakraLink as={Link} to='/postes' fontSize='20px' color='white'>
+            Postes
+          </ChakraLink>
+        </Box>
+        <IconButton
+          aria-label='Menu'
+          icon={<HamburgerIcon />}
+          variant='ghost'
+          color='white'
+          onClick={onOpen}
+          display={['block', 'none']}
+          mr='4'
+        />
+        <Drawer placement='right' onClose={onClose} isOpen={isOpen}>
+          <DrawerOverlay bg='rgba(0, 0, 0, 0.6)' />
+          <DrawerOverlay />
+          <DrawerContent bg='gray.800' color='white'>
+            <DrawerCloseButton />
+            <DrawerBody justifyContent='flex-start'>
+              <Flex flexDirection='column' justifyContent='flex-start' alignItems='flex-start' my='4'>
+                <ChakraLink as={Link} to='/' mb='2'>
+                  Accueil
+                </ChakraLink>
+                <ChakraLink as={Link} to='/champions' mb='2'>
+                  Champions
+                </ChakraLink>
+                <ChakraLink as={Link} to='/postes' mb='2'>
+                  Postes
+                </ChakraLink>
+              </Flex>
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Navbar;
