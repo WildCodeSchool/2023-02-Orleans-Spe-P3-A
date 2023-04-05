700a5c7d3f27d234a4274e574947ee2b9560a1d1
Wilder <alexis.thibault45150@laposte.net>
2023-04-05 17:16:02 +0200

    conflict resolve

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Home from './pages/Home';
+import List from './components/ListChampions';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/champions' element={<List />} />
+      </Routes>
+    </>
   );
 }
 
