fdd58d44823d5081cc6edaa7761095b58c4ba334
Wilder <alexis.thibault45150@laposte.net>
2023-04-07 16:38:58 +0200

    correction done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { Box, Flex, Heading, Text } from '@chakra-ui/react';
 import SearchBar from '../components/SearchBar';
-import imageSrc from '../../src/assets/images/BackgroundImg.png';
+import imageSrc from '../../src/assets/images/background-image.png';
 
 function Home() {
   return (
