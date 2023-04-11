55a56178839326c5eef6f93a15d840a703e86e50
Ric <richard.desnoues@gmail.com>
2023-04-11 12:08:11 +0200

    fix request footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,87 @@
-import { Box, Flex, Text, Image, Center, Link as ChakraLink, Wrap, WrapItem, extendTheme, Stack } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
+import { Box, Flex, Text, Image, Wrap, Stack } from '@chakra-ui/react';
 
-const customTheme = extendTheme({});
+const guys = [
+  {
+    name: 'Richard',
+    photo: './src/assets/smile.svg',
+  },
+  {
+    name: 'Gregory',
+    photo: './src/assets/smile.svg',
+  },
+  {
+    name: 'Alexis',
+    photo: './src/assets/smile.svg',
+  },
+  {
+    name: 'Typhen',
+    photo: './src/assets/smile.svg',
+  },
+];
 
 const Footer = () => {
   return (
     <div>
-      <Box as='footer' bg='black' w='100%' h={{ base: '480px', md: '250px' }} direction={['column', 'column', 'row', 'row']}>
-        <Stack direction={['column-reverse', 'column-reverse', 'row-reverse', 'row-reverse']}>
+      <Box as='footer' bg='black' w='100%' h={{ base: '480px', md: '250px' }} direction={['column', 'column', 'row']}>
+        <Stack direction={['column', 'column', 'row', 'row']}>
+          {/*Trait de sépration */}
+          <Flex
+            height={{ base: '1px', md: '200px' }}
+            w={{ base: '200px', md: '1px' }}
+            border='grey'
+            opacity='0.5'
+            bg='grey'
+            my='auto'
+            alignSelf='center'
+          />
+
+          {/*Box authors-api */}
+          <Flex
+            direction='column'
+            w={{ base: '100%', md: '50%' }}
+            gap='20px'
+            height={{ base: '290px', md: '250px' }}
+            color='white'
+            p={{ base: '10px', md: '30px' }}
+          >
+            {/*Box Authors title */}
+            <Flex
+              justify={['center', 'center', 'flex-start']}
+              w='100%'
+              color='grey'
+              fontSize='2rem'
+              pl={{ base: '0', md: '24px' }}
+              mt={{ base: '10px', md: 'initial' }}
+            >
+              <Text>Authors</Text>
+            </Flex>
+
+            {/*authors name and photo*/}
+            <Wrap w={{ base: '100%' }} h={{ base: '300px' }} justify='space-between'>
+              {/*bloc name photo */}
+              {guys.map(guy => (
+                <Flex w={{ base: '47%', md: '155px' }} justify='center'>
+                  <Flex
+                    justify='space-between'
+                    align='center'
+                    direction={['column', 'column', 'row']}
+                    gap='10px'
+                    w={{ base: '77px', md: '116px' }}
+                    minW='116px'
+                  >
+                    {/*authors photo*/}
+                    <Image w='100%' borderRadius='full' boxSize='40px' src={guy.photo} alt={`photo de ${guy.name}`} />
+
+                    {/*authors name*/}
+                    <Text color='grey' fontSize={{ base: '1.4rem', md: '1.2rem' }} minW='72px' textAlign='start'>
+                      {guy.name}
+                    </Text>
+                  </Flex>
+                </Flex>
+              ))}
+            </Wrap>
+          </Flex>
+
           {/*Api data source*/}
           <Box
             w={{ base: '100%', md: '50%' }}
@@ -18,15 +92,14 @@ const Footer = () => {
           >
             <Flex direction='column' justify={['center', 'center', 'space-around']} h={{ base: '160px', md: '250px' }} pt={{ md: '17px' }}>
               <Flex h='200px' direction='column' justify={{ base: 'space-around', md: 'initial' }}>
-                <Flex justify='flex-start' ml={{ base: '35px', md: '30px' }} fontSize='1.5rem' color='#D0A85C'>
-                  Api League of Legends
-                </Flex>
-                <Flex justify='flex-start'>
-                  <Text fontSize='0.5em' color='grey' ml={{ base: '35px', md: '30px' }} textAlign='start'>
-                    All the content of B2G'S comes from the API of League of Legends Database.
-                  </Text>
-                </Flex>
+                <Text textAlign='start' ml={{ base: '35px', md: '30px' }} fontSize='1.5rem' color='#D0A85C'>
+                  {'API League of Legends'}
+                </Text>
+                <Text fontSize='0.5em' color='grey' ml={{ base: '35px', md: '30px' }} textAlign='start'>
+                  All the content of B2G'S comes from the API of League of Legends Database.
+                </Text>
               </Flex>
+
               <Flex justify='flex-start'>
                 <Flex
                   justify='space-between'
@@ -47,104 +120,12 @@ const Footer = () => {
                     marginBottom={{ base: '20px' }}
                     color='#D0A85C'
                   >
-                    Mentions légales
+                    Legal notice
                   </Text>
                 </Flex>
               </Flex>
             </Flex>
           </Box>
-
-          {/*Trait de sépration */}
-          <Flex
-            height={{ base: '1px', md: '200px' }}
-            w={{ base: '200px', md: '1px' }}
-            border='grey'
-            opacity='0.5'
-            bg='grey'
-            my='auto'
-            alignSelf='center'
-          ></Flex>
-
-          {/*Box authors-api */}
-          <Flex
-            direction='column'
-            w={{ base: '100%', md: '50%' }}
-            gap='20px'
-            height={{ base: '290px', md: '250px' }}
-            color='white'
-            p={{ base: '10px', md: '30px' }}
-          >
-            {/*Box Authors title */}
-            <Flex
-              justify={['center', 'center', 'flex-start']}
-              w='100%'
-              color='grey'
-              fontSize='2rem'
-              pl={{ base: '0', md: '24px' }}
-              mt={{ base: '10px', md: 'initial' }}
-            >
-              Authors
-            </Flex>
-
-            {/*authors name and photo*/}
-            <Wrap w={{ base: '100%' }} h={{ base: '300px' }} justify='space-between'>
-              {/*bloc name photo */}
-              <Flex w={{ base: '47%', md: '155px' }} justify='center' color='white'>
-                <Flex justify='space-between' align='center' direction={['column', 'column', 'row']} gap='10px'>
-                  {/*authors photo*/}
-                  <Center className='photo-profile' w='50%' color='white'>
-                    <Image w='100%' borderRadius='full' boxSize='40px' src='./src/assets/smile.svg' alt='ric photo' />
-                  </Center>
-                  {/*authors name*/}
-                  <Center className='name' color='grey' fontSize={{ base: '1.4rem', md: '1.2rem' }}>
-                    Richard
-                  </Center>
-                </Flex>
-              </Flex>
-
-              {/*bloc name photo */}
-              <Flex w={{ base: '47%', md: '155px' }} justify='center' color='white'>
-                <Flex justify='space-between' align='center' direction={['column', 'column', 'row']} gap='10px'>
-                  {/*authors photo*/}
-                  <Center className='photo-profile' w='50%'>
-                    <Image w='100%' borderRadius='full' boxSize='40px' src='./src/assets/smile.svg' alt='ric photo' />
-                  </Center>
-                  {/*authors name*/}
-                  <Center className='name' color='grey' fontSize={{ base: '1.4rem', md: '1.2rem' }}>
-                    Gregory
-                  </Center>
-                </Flex>
-              </Flex>
-
-              {/*bloc name photo */}
-              <Flex w={{ base: '47%', md: '155px' }} justify='center' pr={{ md: '15px' }} color='white'>
-                <Flex justify='space-between' align='center' direction={['column', 'column', 'row']} gap='10px'>
-                  {/*authors photo*/}
-                  <Center className='photo-profile' w='50%'>
-                    <Image w='100%' borderRadius='full' boxSize='40px' src='./src/assets/smile.svg' alt='ric photo' />
-                  </Center>
-                  {/*authors name*/}
-                  <Center className='name' color='grey' fontSize={{ base: '1.4rem', md: '1.2rem' }} w={{ base: '77px', md: 'initial' }}>
-                    Alexis
-                  </Center>
-                </Flex>
-              </Flex>
-
-              {/*bloc name photo */}
-              <Flex w={{ base: '47%', md: '155px' }} justify='center' color='white'>
-                <Flex justify='space-between' align='center' direction={['column', 'column', 'row']} gap='5px'>
-                  {/*authors photo*/}
-                  <Center w='50%'>
-                    <Image w='100%' borderRadius='full' boxSize='40px' src='./src/assets/smile.svg' alt='ric photo' />
-                  </Center>
-                  {/*authors name*/}
-                  <Center color='grey' fontSize={{ base: '1.4rem', md: '1.2rem' }} w={{ base: '77px', md: 'initial' }}>
-                    Thyphen
-                  </Center>
-                </Flex>
-              </Flex>
-            </Wrap>
-          </Flex>
         </Stack>
       </Box>
     </div>
