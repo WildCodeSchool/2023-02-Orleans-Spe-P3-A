62c5fe40e66ff831340792037b60a9e080d27ad0
SILVESTRI_GREGORY <gregory.silvestri45@gmail.com>
2023-04-11 17:26:02 +0200

    championsFiltering

--- src/pages/ListChampions.jsx
+++ src/pages/ListChampions.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
 import ChampionCard from '../components/ChampionCard';
-import { Box, Heading, Skeleton } from '@chakra-ui/react';
+import { Box, Heading, Skeleton, Button, Select } from '@chakra-ui/react';
 import background from '../assets/images/background-image.png';
 
 function List() {
   const [champions, setChampions] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [filter, setFilter] = useState('');
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
     const abortController = new AbortController();
@@ -32,26 +34,74 @@ function List() {
     };
   }, []);
 
+  const filteredChampions = filter ? champions.filter(champion => champion.tags.includes(filter)) : champions;
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 480);
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <Box backgroundImage={background} backgroundSize='cover' minHeight='100vh' py='4'>
       <Box spacing='4' marginTop='15px'>
         <Heading as='h1' size='2xl' textAlign='center' color='white'>
-          {'Listes des champions'}
+          {'Liste des champions'}
         </Heading>
-        <Box display='flex' justifyContent='flex-start' color='white' marginTop={10} marginLeft='5%'>
-          <Heading as='h2' size='md' mr='4'>
-            {'Tank'}
-          </Heading>
-          <Heading as='h2' size='md' mr='4'>
-            {'Assassin'}
-          </Heading>
-          <Heading as='h2' size='md' mr='4'>
-            {'Mage'}
-          </Heading>
-          <Heading as='h2' size='md' mr='4'>
-            {'Marksman'}
-          </Heading>
-        </Box>
+        {isMobile ? (
+          <Box display='flex' justifyContent='center' color='white' marginTop={10}>
+            <Select
+              width='75%'
+              value={filter}
+              onChange={event => setFilter(event.target.value)}
+              bg='blue.800'
+              borderColor='blue.800'
+              color='white'
+            >
+              <option value=''>{'Tous les champions'}</option>
+              <option value='Tank'>{'Tank'}</option>
+              <option value='Assassin'>{'Assassin'}</option>
+              <option value='Mage'>{'Mage'}</option>
+              <option value='Marksman'>{'Marksman'}</option>
+            </Select>
+          </Box>
+        ) : (
+          <Box display='flex' justifyContent='flex-start' color='black' marginTop={50} marginLeft='4.3%' mb={5}>
+            <Button size='md' mr='4' isActive={filter === ''} onClick={() => setFilter('')} variant='variantButton'>
+              {'Tous les champions'}
+            </Button>
+            <Button size='md' mr='4' isActive={filter === 'Tank'} onClick={() => setFilter('Tank')} variant='variantButton'>
+              {'Tank'}
+            </Button>
+            <Button
+              size='md'
+              mr='4'
+              isActive={filter === 'Assassin'}
+              onClick={() => setFilter('Assassin')}
+              bg='blue.800'
+              color='white'
+              _hover={{
+                backgroundColor: 'blue.700',
+              }}
+              _active={{
+                bg: 'blue.500',
+                color: 'white',
+              }}
+            >
+              {'Assassin'}
+            </Button>
+            <Button size='md' mr='4' isActive={filter === 'Mage'} onClick={() => setFilter('Mage')} variant='variantButton'>
+              {'Mage'}
+            </Button>
+            <Button size='md' mr='4' isActive={filter === 'Marksman'} onClick={() => setFilter('Marksman')} variant='variantButton'>
+              {'Marksman'}
+            </Button>
+          </Box>
+        )}
         <Box display='flex' justifyContent='center' flexWrap='wrap'>
           {isLoading
             ? Array.from({ length: 10 }).map((_, index) => (
@@ -65,7 +115,7 @@ function List() {
                   endColor='blue.800'
                 />
               ))
-            : champions.map(champion => <ChampionCard key={champion.id} champion={champion} />)}
+            : filteredChampions.map(champion => <ChampionCard key={champion.id} champion={champion} />)}
         </Box>
       </Box>
     </Box>
---
+++ src/theme/theme.js
@@ -0,0 +1,22 @@
+import { defineStyle, defineStyleConfig, extendTheme } from "@chakra-ui/react";
+
+const variantButton = defineStyle({
+    bg: 'blue.800',
+    color: 'white',
+    _hover: {
+        backgroundColor: 'blue.700'
+    },
+    _active: {
+        background: 'blue.500',
+        color: 'white'
+    }
+});
+
+const buttonTheme = defineStyleConfig({
+    variants: { variantButton },
+})
+
+export const theme = extendTheme({
+    components: { Button: buttonTheme },
+})
+
