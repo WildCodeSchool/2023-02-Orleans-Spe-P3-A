d1d4614e3da1195388d7d7063d6c6a36780cc182
SILVESTRI_GREGORY <gregory.silvestri45@gmail.com>
2023-04-12 10:02:15 +0200

    champion filter

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import {theme} from './theme/theme'
+import { theme } from './theme/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,22 +1,21 @@
-import { defineStyle, defineStyleConfig, extendTheme } from "@chakra-ui/react";
+import { defineStyle, defineStyleConfig, extendTheme } from '@chakra-ui/react';
 
 const variantButton = defineStyle({
-    bg: 'blue.800',
+  bg: 'blue.800',
+  color: 'white',
+  _hover: {
+    backgroundColor: 'blue.700',
+  },
+  _active: {
+    background: 'blue.500',
     color: 'white',
-    _hover: {
-        backgroundColor: 'blue.700'
-    },
-    _active: {
-        background: 'blue.500',
-        color: 'white'
-    }
+  },
 });
 
 const buttonTheme = defineStyleConfig({
-    variants: { variantButton },
-})
+  variants: { variantButton },
+});
 
 export const theme = extendTheme({
-    components: { Button: buttonTheme },
-})
-
+  components: { Button: buttonTheme },
+});
