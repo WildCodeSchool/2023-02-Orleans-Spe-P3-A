4ff62c4104440b38736be8d9257d2056c471a9ce
Wilder <alexis.thibault45150@laposte.net>
2023-04-13 12:36:37 +0200

    correction done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,6 +10,7 @@ import {
   DrawerContent,
   DrawerCloseButton,
   DrawerBody,
+  Image,
 } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import { HamburgerIcon } from '@chakra-ui/icons';
@@ -22,7 +23,7 @@ const Navbar = () => {
       <Flex alignItems='center' justifyContent='space-between' py='3'>
         <Box fontWeight='bold' color='white' ml='4'>
           <ChakraLink as={Link} to='/'>
-            <img src={logo} alt='Logo' width='110px' />
+            <Image src={logo} alt='Logo' width='110px' />
           </ChakraLink>
         </Box>
         <Box display={['none', 'block']} color='white' fontSize='20px' mr='4'>
